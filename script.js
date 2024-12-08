12e858551911d189eb06702bda14d9394cdeac50
Ahmad Zaeladi <149105143+Ahmadzaeladi@users.noreply.github.com>
2024-12-08 07:34:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal("h1, p, .balon", { origin: "left" });
+ScrollReveal().reveal("a, .kue", { origin: "right" });
+ScrollReveal().reveal("h3", { origin: "bottom" });
+ScrollReveal().reveal("h2", { origin: "top" });
